8c25a0fedd2588096acb6fe3f22968eac73b8b0a
Will <willblev@gmail.com>
2015-11-17 11:31:55 +0100

    Update door.lua

--- src/door.lua
+++ src/door.lua
@@ -1,6 +1,7 @@
 pin = 4
 gpio.mode(pin, gpio.INPUT, gpio.PULLUP)
 currentStatus=''
+thingspeakAPIkey='enter your write API key for the new channel'
 
 
 
@@ -18,7 +19,7 @@ print("Sending data to thingspeak.com")
 conn=net.createConnection(net.TCP, 0) 
 conn:on("receive", function(conn, payload) print(payload) end)
 conn:connect(80,'184.106.153.149') 
-conn:send("GET /update?key=YOURAPIKEY&field1="..currentStatus.." HTTP/1.1\r\n") 
+conn:send("GET /update?key="..thingspeakAPIkey.."&field1="..currentStatus.." HTTP/1.1\r\n") 
 conn:send("Host: api.thingspeak.com\r\n") 
 conn:send("Accept: */*\r\n") 
 conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
