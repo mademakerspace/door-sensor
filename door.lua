f90f2cbade28eee25694f50fee32df6b0e5d9dfa
willblev <willblev@gmail.com>
2015-11-17 07:13:35 +0100

    small edit (changed evaluator to 1 instead of gpio.LOW)

--- door.lua
+++ door.lua
@@ -2,12 +2,12 @@ gpio.mode(4, gpio.INPUT, gpio.PULLUP)
 srv=net.createServer(net.TCP) 
 door='null'
 srv:listen(80,function(conn) conn:on("receive",function(conn,payload)
-if gpio.read(4)==gpio.LOW then
+if gpio.read(4)==1 then
      door ="OPEN"  
 else 
      door="CLOSED" 
 end
-conn:send("Made is " .. door ..".")
+conn:send("Made is currently " .. door ..".")
 end)
 conn:on("sent",function(conn) conn:close() end)
 end)
