3627de3455e37a064cabd2f0aef7e2f7813d6be8
Will <willblev@gmail.com>
2015-11-17 11:40:36 +0100

    changed pushed value to int (1 or 0) instead of text

--- src/door.lua
+++ src/door.lua
@@ -1,15 +1,18 @@
+thingspeakAPIkey='enter your Write API key for the new channel'
 pin = 4
+
+
 gpio.mode(pin, gpio.INPUT, gpio.PULLUP)
-currentStatus=''
-thingspeakAPIkey='enter your write API key for the new channel'
-
-
+currentStatus='OPEN'
+currentValue=1
 
 function getStatus()
 	if gpio.read(4)==1 then
 		currentStatus='OPEN'
+		currentValue=1
 	else
 		currentStatus='CLOSED'
+		currentValue=0
    end
 end
 
@@ -19,7 +22,7 @@ print("Sending data to thingspeak.com")
 conn=net.createConnection(net.TCP, 0) 
 conn:on("receive", function(conn, payload) print(payload) end)
 conn:connect(80,'184.106.153.149') 
-conn:send("GET /update?key="..thingspeakAPIkey.."&field1="..currentStatus.." HTTP/1.1\r\n") 
+conn:send("GET /update?key="..thingspeakAPIkey.."&field1="..currentValue.." HTTP/1.1\r\n") 
 conn:send("Host: api.thingspeak.com\r\n") 
 conn:send("Accept: */*\r\n") 
 conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
