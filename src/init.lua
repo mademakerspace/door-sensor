675badc8795264c2ec4f646060a299a9bd9bd472
Will <willblev@gmail.com>
2015-11-17 11:22:00 +0100

    thingspeak version

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,18 @@
+print("Setting up WIFI...")
 wifi.setmode(wifi.STATION)
-wifi.sta.config('made-bcn.org','@MADEBCN@')
+wifi.sta.config("made-bcn.org","@MADEBCN@")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function()
+if wifi.sta.getip()== nil then
+print("IP unavaiable, Waiting...")
+else
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
 file.remove("door.lc")
 node.compile("door.lua") 
 dofile("door.lc")
+end
+end)
+
+
      
